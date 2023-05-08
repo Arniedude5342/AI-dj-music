98c9615df97a97333641e446fc115acbfc19af82
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2023-05-07 19:01:50 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 song1 = "";
 song2 = "";
 song1_status = "";
-song1_status = "";
+song2_status = "";
 
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -24,6 +25,7 @@ function gotPoses(results)
 {
     if(results.length > 0){
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist = "+ scoreLeftWrist);
 
@@ -57,10 +59,19 @@ function draw() {
         if(song1_status == false)
         {
             song1.play();
-            ducument.getElementById("song").innerHTML = "Playing Peter Pan Song";
+            ducument.getElementById("song").innerHTML = "Playing star song";
         }
     }
-    
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        song1.stop();
+    }
+    if(song2_status = false)
+    {
+        song2.play();
+        document.getElementById("song").innerHTML = "playing Peter Pan Song"
+    }
     
 }
 
